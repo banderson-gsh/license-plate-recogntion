ac41241299b7ae5ff462527fdefc6f52456e8f4c
Brian Anderson <lwitico.anderson@gmail.com>
2023-09-14 11:41:32 +0200

    added params, query strings to the endpoint url

--- utils/anpr.py
+++ utils/anpr.py
@@ -1,31 +1,50 @@
 import httpx
 import base64
 from core.config import settings
+import logging
+from urllib.parse import urlencode, urljoin
 
-async def send_to_anpr(image_data: bytes):
+logging.basicConfig(level=logging.INFO)
+
+async def send_to_anpr(image_data: str):
     basic_auth = base64.b64encode(f"{settings.ANPR_API_USER}:{settings.ANPR_API_PASSWORD}".encode()).decode()
     headers = {
         "Authorization": f"Basic {basic_auth}",
-        "Content-Type": "application/json"
+        "Content-Type": "text/plain"
     }
-
-    payload = {
-        "image": base64.b64encode(image_data).decode()
+    
+    parameters = {
+        'region': 'EU',
+        'topn': 10,
+        'confidence': 400
     }
+    
+    query_string = urlencode(parameters)
+    full_url = urljoin(settings.ANPR_API_ENDPOINT, f"?{query_string}")
+    
+    payload = image_data
+    license_number = None
 
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.post(settings.ANPR_API_ENDPOINT, json=payload, headers=headers)
+            logging.info(f"Sending request to {full_url} with headers {headers} and payload {payload[:500]}...")
+            response = await client.post(full_url, data=payload, headers=headers)
             response.raise_for_status()
             response_json = response.json()
 
-            plate_number = response_json.get("vehicle")["plate"]["license"] if response_json.get("vehicle") else None
-            return plate_number
+            logging.info("response_json:", response_json)
+
+            if response_json.get("vehicle") and len(response_json["vehicle"]) > 0:
+                license_number = response_json["vehicle"][0]["plate"]["license"]
+            else:
+                license_number = None
+                
+            return license_number
         
         except httpx.HTTPStatusError as e:
-            print(f"HTTP error occurred: {e}")
+            logging.error(f"HTTP error occurred: {e}")
         except httpx.RequestError as e:
-            print(f"Request error occurred: {e}")
+            logging.error(f"Request error occurred: {e}")
         except ValueError as e:
-            print(f"Recognition failure: {e}")
-
+            logging.error(f"Recognition failure: {e}")
+            
